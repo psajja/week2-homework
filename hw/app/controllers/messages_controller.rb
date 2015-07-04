fa023bcbcfd96223ea48a80236b1c159b99c800a
psajja <psajja@uchicago.edu>
2015-07-04 12:27:00 -0500

    Hw2

--- hw/app/controllers/messages_controller.rb
+++ hw/app/controllers/messages_controller.rb
@@ -1,7 +1,30 @@
 class MessagesController < ApplicationController
 
+  # Display hello / name of the person
   def hello
     render "show"
   end
   
+  # Request contact information from the user
+  def contactrequest
+    render "requestcontactinfo"
+  end
+
+
+  # Display the contact information
+  def contactdisplay
+    render "displaycontactinfo"
+  end
+
+
+  # Request for a city name
+  def weatherrequest
+    render "requestweatherinfo"
+  end
+
+  # Display the weather information of that city
+  def weatherdisplay
+    render "displayweatherinfo"
+  end
+
 end
--- hw/config/routes.rb
+++ hw/config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
 
+  #Route for saying hello / Name of the person
   get("/greet", :controller => 'messages', :action => 'hello')
+
+  #Route for requesting the contact information
+  get("/contact", :controller => 'messages', :action => 'contactrequest')
+
+  #Route for displaying the contact information
+  get("/contact_submitted", :controller => 'messages', :action => 'contactdisplay')
+
+
+  #Route for the weather form
+  get("/weather", :controller => 'messages', :action => 'weatherrequest')
+
+
+ #Route for displaying the weather of a location
+  get("/current_conditions", :controller => 'messages', :action => 'weatherdisplay')
+
 
 end
